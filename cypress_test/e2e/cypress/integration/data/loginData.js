a59c9968a2643b03d8668354ee4b60ccccbbdc3d
Shruthi Mukunda <kmshruthi@gmail.com>
2022-02-15 14:21:35 +0000

    remove unwanted files

--- cypress_test/e2e/cypress/integration/data/loginData.js
+++
@@ -1,8 +0,0 @@
-const login_data = Object.freeze({
-    USERNAME: Cypress.env().Username,
-    PASSWORD: Cypress.env().Password,
-    HEADER: "SWAGLABS"
-  });
-  
-  export default login_data;
-  
--- cypress_test/e2e/cypress/integration/data/sortingFilterData.js
+++
@@ -1,8 +0,0 @@
-const login_data = Object.freeze({
-    first_name: "First name",
-    last_name: "Last name",
-    zip_code: "AB1 1CD"
-  });
-  
-  export default login_data;
-  
